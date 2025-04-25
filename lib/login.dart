5a7cf4c4738980893b243d1d21606c1292437766
Admin <sofiia.vynar.ir.2022@lpnu.ua>
2025-04-25 09:05:52 +0300

    Commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:labb_1/home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -12,12 +15,33 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  late Connectivity _connectivity;
+  late StreamSubscription<ConnectivityResult> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _connectivity = Connectivity();
+    _subscription = _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.none && mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Немає інтернету')),
+        );
+      }
+    });
+    _checkAutoLogin();
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
 
   Future<bool> _checkUserCredentials(String email, String password) async {
     final prefs = await SharedPreferences.getInstance();
     final storedEmail = prefs.getString('user_email');
     final storedPassword = prefs.getString('user_password');
-
     return storedEmail == email && storedPassword == password;
   }
 
@@ -45,6 +69,15 @@ class _LoginScreenState extends State<LoginScreen> {
     final email = _emailController.text;
     final password = _passwordController.text;
 
+    final connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Немає з’єднання з Інтернетом')),
+      );
+      return;
+    }
+
     if (email.isEmpty || password.isEmpty) {
       if (!mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
@@ -57,7 +90,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
     if (isValid) {
       final prefs = await SharedPreferences.getInstance();
-      await prefs.setBool('isLoggedIn', true);
+      await prefs.setBool('is_logged_in', true);
 
       if (!mounted) return;
       Navigator.pushAndRemoveUntil<void>(
@@ -73,6 +106,28 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  Future<void> _checkAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isLoggedIn = prefs.getBool('is_logged_in') ?? false;
+
+    if (isLoggedIn) {
+      final connectivityResult = await Connectivity().checkConnectivity();
+      if (connectivityResult == ConnectivityResult.none && mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Автологін без Інтернету – обмежений функціонал'),
+          ),
+        );
+      }
+
+      if (!mounted) return;
+      Navigator.pushAndRemoveUntil<void>(
+        context,
+        MaterialPageRoute<void>(builder: (context) => const HomeScreen()),
+            (route) => false,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/sensors.dart
+++ lib/sensors.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
 class SensorsScreen extends StatefulWidget {
   const SensorsScreen({super.key});
@@ -13,12 +16,16 @@ class SensorsScreenState extends State<SensorsScreen> {
   final Random _random = Random();
   double _voltage = 0;
   double _power = 0;
+  String _voltageFromMqtt = '--';
+
+  late MqttServerClient _client;
 
   @override
   void initState() {
     super.initState();
     _updateSensorData();
     Timer.periodic(const Duration(seconds: 10), (timer) => _updateSensorData());
+    _connectToMQTT();
   }
 
   void _updateSensorData() {
@@ -26,6 +33,48 @@ class SensorsScreenState extends State<SensorsScreen> {
       _voltage = 210 + _random.nextDouble() * 30;
       _power = 100 + _random.nextDouble() * 900;
     });
+  }
+
+  Future<void> _connectToMQTT() async {
+    _client = MqttServerClient('test.mosquitto.org', 'client_1');
+    _client.port = 1883;
+    _client.logging(on: true);
+    _client.keepAlivePeriod = 20;
+    _client.onDisconnected = _onDisconnected;
+    _client.onConnected = _onConnected;
+
+    try {
+      await _client.connect();
+    } catch (e) {
+      if (kDebugMode) {
+        print('MQTT connection failed: $e');
+      }
+      _client.disconnect();
+    }
+  }
+
+
+  void _onConnected() {
+    if (kDebugMode) {
+      print('MQTT Connected');
+    }
+    _client.subscribe('sensor/voltage', MqttQos.atMostOnce);
+
+    _client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> messages) {
+      final recMess = messages[0].payload as MqttPublishMessage;
+      final payload = MqttPublishPayload.bytesToStringAsString
+        (recMess.payload.message);
+
+      setState(() {
+        _voltageFromMqtt = payload;
+      });
+    });
+  }
+
+  void _onDisconnected() {
+    if (kDebugMode) {
+      print('MQTT Disconnected');
+    }
   }
 
   Widget _sensorCard(String label, String value, IconData icon) {
@@ -57,6 +106,9 @@ class SensorsScreenState extends State<SensorsScreen> {
             _sensorCard('Потужність',
                 '${_power.toStringAsFixed(2)} Вт',
                 Icons.electric_bolt,),
+            _sensorCard('Напруга з MQTT',
+                '$_voltageFromMqtt В',
+                Icons.flash_on,),
           ],
         ),
       ),
